cb77e2c05b87402cb6f30ec9040c1667ee753e1e
Igor Cassolli <igorcassolli1305@gmail.com>
2024-04-29 14:30:23 -0300

    :tada: Setup Initial

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,32 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { Container } from "./styles";
+import {
+    Container,
+    Header,
+    Logo,
+    UserPhoto,
+    CardResult,
+    TextLabel,
+    CardResultLabelIcon,
+    CardResultTotalPercent,
+    CardResultLabel
+} from "./styles";
+import LogoImg from "../../assets/logo.png";
 
 export default function Home() {
     return (
-
         <Container>
-            <Text>oiiiiiiiiiiiiiii</Text>
+            <Header>
+                <Logo source={LogoImg} />
+                <UserPhoto
+                    source={{ uri: "https://avatars.githubusercontent.com/u/37945750?v=4" }}
+                />
+            </Header>
+            <CardResult>
+                <CardResultLabelIcon></CardResultLabelIcon>
+                <CardResultTotalPercent></CardResultTotalPercent>
+                <CardResultLabel></CardResultLabel>
+            </CardResult>
+            <TextLabel></TextLabel>
         </Container>
-
-
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,37 @@
 import styled from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
     flex: 1;
-    justify-content: center;
+    padding-top: 64px;
+    padding-right: 24px;
+    padding-left: 24px;
+`
+export const Header = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
     align-items: center;
+`
+export const Logo = styled.Image`
+    width: 82px;
+    height: 37px;
+`
+export const UserPhoto = styled.Image`
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+`
+export const CardResult = styled.View`
+
+`
+export const CardResultLabelIcon = styled.Icon`
+
+`
+export const CardResultTotalPercent = styled.Text`
+`
+export const CardResultLabel = styled.Text`
+`
+export const TextLabel = styled.Text`
+
 `
